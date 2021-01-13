924118e525f52351a70c2937679e9cf279360c41
LittleKingsguard <rarasey@outlook.com>
2021-01-13 13:09:55 -0600

    Update Feline.java

--- abstract class test/src/Animals/Feline.java
+++ abstract class test/src/Animals/Feline.java
@@ -23,9 +23,9 @@ public class Feline extends Animal{
 		System.out.print("The feline travels from " + Arrays.toString(this.location));
 		Random rand = new Random(System.currentTimeMillis());
 		int dimension = this.boundaries[0][1]-this.boundaries[0][0];
-		this.location[0] = rand.nextInt() % dimension + this.boundaries[0][0];
+		this.location[0] = Math.abs(rand.nextInt()) % dimension + this.boundaries[0][0];
 		dimension = this.boundaries[1][1]-this.boundaries[1][0];
-		this.location[1] = rand.nextInt() % dimension + this.boundaries[1][0];
+		this.location[1] = Math.abs(rand.nextInt()) % dimension + this.boundaries[1][0];
 		System.out.print(" to  " + Arrays.toString(this.location));
 		System.out.println();
 	}
